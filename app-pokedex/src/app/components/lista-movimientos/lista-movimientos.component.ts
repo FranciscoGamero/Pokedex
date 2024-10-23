c6b58597e29d0c67f172b2d242c0d8fede98c7f0
alcantarilla.cafra24 <alcantarilla.cafra24@triana.salesianos.edu>
2024-10-23 12:25:54 +0200

    Lista de los movimientos

--- app-pokedex/src/app/components/lista-movimientos/lista-movimientos.component.ts
+++ app-pokedex/src/app/components/lista-movimientos/lista-movimientos.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movimiento } from '../../models/movimiento.interface';
+import { MovimientoService } from '../../services/movimiento.service';
 
 @Component({
   selector: 'app-lista-movimientos',
   templateUrl: './lista-movimientos.component.html',
   styleUrl: './lista-movimientos.component.css'
 })
-export class ListaMovimientosComponent {
+export class ListaMovimientosComponent implements OnInit {
 
+  listaMovimiento: Movimiento[]=[]
+
+  constructor(private movimientoServicio: MovimientoService){}
+
+  ngOnInit(): void {
+      this.movimientoServicio.getListaMovimiento().subscribe((resp)=>{
+        this.listaMovimiento = resp.results;
+      })
+  }
 }
