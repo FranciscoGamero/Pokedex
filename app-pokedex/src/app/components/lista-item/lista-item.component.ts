2bc482164960ab2c2f7be1ba8b7de55ef612d8d0
Francisco Manuel <gamero.rofra24@triana.salesianos.edu>
2024-10-23 10:09:27 +0200

    Se agregan las bases para empezar a trabajar con angular en el proyecto

---
+++ app-pokedex/src/app/components/lista-item/lista-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-item',
+  templateUrl: './lista-item.component.html',
+  styleUrl: './lista-item.component.css'
+})
+export class ListaItemComponent {
+
+}
---
+++ app-pokedex/src/app/components/lista-movimientos/lista-movimientos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-movimientos',
+  templateUrl: './lista-movimientos.component.html',
+  styleUrl: './lista-movimientos.component.css'
+})
+export class ListaMovimientosComponent {
+
+}
